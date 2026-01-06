709bf6df0fc735b551e8d45f53076b482d8a7fb3
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2026-01-07 00:38:33 +0530

    Data Ingestion Configration Completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -23,5 +23,12 @@ class DataIngestionConfig:
             self.data_ingestion_dir,training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
         )
         self.training_file_path:str=os.path.join(
-            self.data_ingestion_dir,training_pipeline.
+            self.data_ingestion_dir,training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
         )
+        self.testing_file_path:str=os.path.join(
+            self.data_ingestion_dir,training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
+        )
+
+        self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+        self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
